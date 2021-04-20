8c1b348f48fbfeca3c71c85aa7e6b120101fc993
dennisgbrown <dennis.g.brown@gmail.com>
2021-04-19 20:35:07 -0500

    set up experiment

---
+++ run_experiment.sh
@@ -0,0 +1,6 @@
+python code/start.py configs/a.cfg
+python code/start.py configs/b.cfg
+python code/start.py configs/c.cfg
+python code/start.py configs/d.cfg
+python code/start.py configs/e.cfg
+python code/start.py configs/f.cfg
